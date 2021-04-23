131d77da6a6151545e66e1dafe8b639ad89b9ac6
Lars Rückert <lars.rueckert@data4life.care>
2021-04-23 09:33:18 +0200

    🔧 K8s access certs (#9)

--- jenkinsfile/build.groovy
+++ jenkinsfile/build.groovy
@@ -1,6 +1,6 @@
 #!groovy
 
-@Library(value='pipeline-lib@v2.6.0', changelog=false) _
+@Library(value='pipeline-lib@v2.14.0', changelog=false) _
 
 buildPipeline projectName: 'go-svc-template',
     namespace: 'default',
